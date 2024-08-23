aaf6c1b9cdce9c57d0f23e24c8da454c5ab6f8df
Davuks <javascript12300000@gmail.com>
2024-08-23 22:01:09 +0800

    Update delete_plugin.bat

--- delete_plugin.bat
+++ delete_plugin.bat
@@ -1,25 +1,10 @@
 @echo off
 
-REM Находим процесс Gorilla Tag.exe
-for /f "tokens=2 delims=," %%a in ('tasklist /FI "IMAGENAME eq Gorilla Tag.exe" /FO CSV /NH') do (
-    set PID=%%a
-)
+REM Удаляем DLL
+del /f /q "%~dp0BepInEx\plugins\utgload.dll"
 
-if not defined PID (
-    echo Process Gorilla Tag.exe not found.
-    exit /b
-)
-
-REM Находим путь к исполняемому файлу процесса
-for /f "tokens=2 delims==" %%a in ('wmic process where "ProcessId=%PID%" get ExecutablePath /value') do (
-    set EXEPATH=%%a
-)
-
-REM Переходим в директорию игры
-cd /d "%EXEPATH%\.."
-
-REM Удаляем плагин
-del /f /q "BepInEx\plugins\utgload.dll"
+REM Удаляем маркерный файл, чтобы вернуть систему в исходное состояние
+del /f /q "%~dp0BepInEx\plugins\plugin_installed.marker"
 
 REM Завершаем процесс Gorilla Tag.exe
-taskkill /PID %PID%
+taskkill /IM "Gorilla Tag.exe" /F
